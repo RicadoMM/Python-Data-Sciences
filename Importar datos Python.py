4e64037d27387b8f465488e5fdb6b4c93ce5a1bb
RicardoMartinezM <40036515+RicadoMM@users.noreply.github.com>
2020-03-13 22:00:17 +0100

    Create Importar datos Python.py

---
+++ Importar datos Python.py
@@ -0,0 +1,8 @@
+"""
+En primer lugar debemos de llevar a cabo el analisis de datos mediante python. 
+Una vez introducidos los conceptos aplicables y que nos valdran para le tramtamiento de datasets 
+(la mayoria de llos vendran en un formato conocido como CSV,Excel,JSON,XML).
+Para ello vamos a dividir este articulo en los siguientes puntos que iremos explicando a continuacion:
+1. Importacion de datos con Python.
+2.
+"""
