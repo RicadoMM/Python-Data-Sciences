a2396ac6a5ed0e1c8db301bd1875f4a26229a9be
RicardoMartinezM <40036515+RicadoMM@users.noreply.github.com>
2020-03-13 22:57:00 +0100

    Update Importar datos Python.py

--- Importar datos Python.py
+++ Importar datos Python.py
@@ -5,4 +5,12 @@ Una vez introducidos los conceptos aplicables y que nos valdran para le tramtami
 Para ello vamos a dividir este articulo en los siguientes puntos que iremos explicando a continuacion:
 1. Importacion de datos con Python.
 2.
+
+IMPORTACION DE DATOS CON PYTHON
+
+Tenemos varios metodos para importar los datos desde fichero. El primero de los casos sera desde un fichero CSV.
+Metodo de read_csv:
 """
+import pandas as pd
+import numpy as np
+df = pd.read_csv(path) 
