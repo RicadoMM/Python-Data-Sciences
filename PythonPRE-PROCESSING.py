12e8c137bd8acaea954c4d5a72ce156892d4bac3
RicardoMartinezM <40036515+RicadoMM@users.noreply.github.com>
2020-03-16 18:28:18 +0100

    Create PythonPRE-PROCESSING.py

---
+++ PythonPRE-PROCESSING.py
@@ -0,0 +1,20 @@
+"""
+En este caso vamos a evaluar las técnicas de pre-procesado de datos, un punto muy importante para poder seguir avanzando en las técnicas de machine learning, deep learning
+data-sciences, etc, se trata de un punto importante. Se suele dedicar la mayoria del tiempo a este apartado.
+
+
+Técncias que empleadas:
+  1. Identificar y manejar valores que no existen -- Missing Values
+  2. Formato de los datos -- Data Formatting
+  3. Normalizacion de los datos -- Data Normalization(centering/scaling)
+  4. Data Binning
+  5. Paso de datos categoricos a datos numericos.
+  
+  Si abordamos un poco los pasos que se tienen que realizar en le pre-procesado de datos en machine learning:
+  1.Cleanning.
+  2.Integration.
+  3.Reduction.
+  4.Transformation.
+  5.Discretizacion.
+  
+"""
