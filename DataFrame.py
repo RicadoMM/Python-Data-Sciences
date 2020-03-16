bb25b00c5c2184fd8f2e984b03af985eb160d13d
RicardoMartinezM <40036515+RicadoMM@users.noreply.github.com>
2020-03-17 00:31:07 +0100

    Create DataFrame.py

---
+++ DataFrame.py
@@ -0,0 +1,9 @@
+"""
+Se trata de uno de los elementos mas empleados en el uso diario de Python, se puede definir a grandes rasgos como un conjunto de filas y de columnas
+que se recogen en una tabla con o sin cabeceras y con indice.
+
+Los dataFrame se pueden crear partiendo de diccinarios o de arrays creados con Numpy, asi como al realizar una consulta SQL contra SQL SERVER
+o importando un fichero csv con pandas.read_csv, tambien al importar un fichero excel con pandas.read_excel.
+
+El dataFrame se recoge dentro de la libreria Pandas y se puede entender como dijimos como una tabla, pudiendo recorrer sus filas, sus columnas,
+pudiendo 
